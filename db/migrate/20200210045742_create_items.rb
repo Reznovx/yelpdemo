b118bc2594a93690795926c44ba0e3194dddbf3f
Reznovx <rezakhareisa@gmail.com>
2020-02-10 17:16:38 +0700

    fist commit

---
+++ db/migrate/20200210045742_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :address
+      t.string :phone
+      t.string :website
+
+      t.timestamps
+    end
+  end
+end
